84e4e8011b833feb6042909d5d24caf1b8615350
Sella Rafaeli <sella.rafaeli@gmail.com>
2020-04-24 00:34:20 -0700

    views - store _req.referrer

--- bl/views.rb
+++ bl/views.rb
@@ -1,5 +1,7 @@
 $views = $mongo.collection('views')
 
 def record_view(post_id, user_id)
-	$views.add({post_id: post_id, user_id: user_id, device_id: session[:device_id]})
+  data = {post_id: post_id, user_id: user_id, device_id: session[:device_id]}
+  data[:referrer] = _req.referrer.to_s rescue ''
+  $views.add(data)
 end
