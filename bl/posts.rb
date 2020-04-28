c94976280e6fb540fbd112f8794e434f3eb6abc8
Sella Rafaeli <sella.rafaeli@gmail.com>
2020-04-27 21:42:08 -0700

    auto-message

--- bl/posts.rb
+++ bl/posts.rb
@@ -23,7 +23,8 @@ end
 
 def get_post_og_img(post)
 	#return 'https://תוכן.com/favicon.ico'
-	return 'https://www.xn--9dbmm5b.com/favicon.ico'
+	#return 'https://www.xn--9dbmm5b.com/favicon.ico'
+	return 'https://www.xn--9dbmm5b.com/img/taf.png'
 	para = (post[:ps].to_a.find {|p| p['type'] == 'img' } || {})
 	para['src'] || 'https://תוכן.com/favicon.ico'
 end
